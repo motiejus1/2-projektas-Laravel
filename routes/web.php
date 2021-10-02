2ce2952f503cab5e4fe3dc669d98591b95fcbff5
Motiejus <motiejus.baliukonis@vka.lt>
2021-10-02 12:34:47 +0300

    *laravel 8 fix

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,12 @@ Route::get('/test', function () {
 
 Route::prefix('authors')->group(function () {
     // mes sukursime grupe keliu, kurie susije su autoriais
+
+    //Jeigu raso, kad controller nerastas
     // Route::get('','App\Http\Controllers\AuthorController@index')->name('author.index');
+    // Route::get('create', 'App\Http\Controllers\AuthorController@create')->name('author.create');
+    // Route::post('store', 'App\Http\Controllers\AuthorController@store')->name('author.store');
+
     Route::get('','AuthorController@index')->name('author.index');
     Route::get('create', 'AuthorController@create')->name('author.create');
     Route::post('store', 'AuthorController@store')->name('author.store');
